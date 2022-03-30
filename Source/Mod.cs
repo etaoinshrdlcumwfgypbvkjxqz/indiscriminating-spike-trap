34c1bc89e0373e38572f1e3041f1a56974dc8424
William So <polyipseity@gmail.com>
2022-03-30 16:16:37 +0800

    Complete the mod
    Signed-off-by: William So <polyipseity@gmail.com>

--- Source/Mod.cs
+++ Source/Mod.cs
@@ -1,7 +1,14 @@
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using RimWorld;
 using Verse;
+using HarmonyLib;
 
 namespace Polyipseity.IndiscriminatingSpikeTrap {
 	public class EarlyModStartup : Mod {
+		internal static readonly Harmony HARMONY = new Harmony(ModStartup.ID);
+
 		public EarlyModStartup(ModContentPack content) : base(content) {}
 	}
 
@@ -9,6 +16,31 @@ namespace Polyipseity.IndiscriminatingSpikeTrap {
 	public static class ModStartup {
 		public const string ID = nameof(Polyipseity.IndiscriminatingSpikeTrap);
 
-		static ModStartup() {}
+		static ModStartup() {
+			EarlyModStartup.HARMONY.PatchAll();
+			Log.Message($"[{ID}] Patches applied.");
+		}
+	}
+
+	[HarmonyPatch(typeof(Building_Trap), "SpringChance")]
+	internal static class Patch_NullifyKnowledgeeOfTrap {
+		private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) {
+			bool transpiling = true;
+			foreach (var instruction in instructions) {
+				yield return instruction;
+				if (transpiling && instruction.opcode == OpCodes.Call) {
+					MethodInfo operand = (MethodInfo) instruction.operand;
+					if (operand.ReturnType == typeof(bool) && operand.DeclaringType == typeof(Building_Trap)) {
+						var parameters = operand.GetParameters();
+						if (parameters.Length == 1 && parameters[0].ParameterType == typeof(Pawn)) {
+							// BuildingTrap::(?): Pawn -> bool
+							yield return new CodeInstruction(OpCodes.Pop); // discard the result
+							yield return new CodeInstruction(OpCodes.Ldc_I4_0); // load false
+							transpiling = false;
+						}
+					}
+				}
+			}
+		}
 	}
 }
